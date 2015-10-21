ee24334c2382bde722ee1bbe8840777e1c804397
Geraldine <geraldinegalue@gmail.com>
2015-10-21 10:59:34 -0400

    program is working

--- student_shuffle.rb
+++ student_shuffle.rb
@@ -1,13 +1,38 @@
-students = %w(Geraldine Janeth Gio Carlos Richard Jackeline Beatriz Angie Aaron)
-#%w(list items) makes an array
+# students = %w(Geraldine Janeth Gio Carlos Richard Jackeline Angie Aaron)
+# #%w(list items) makes an array
+
+students = []
+complete = false
+
+puts "Who came to class today (type 'done' when finished)?"
+until complete == true
+	name = gets.chomp.downcase.capitalize
+	if name == "Done"
+		complete = true
+	else
+		students.push(name)
+	end
+end 
 
 students.shuffle! 
 
 count = 0
+num_students = students.length 
 
-until count == (students.length - 1)
+until count == num_students
 
-	puts "#{students[count]} & #{students[count + 1]}"
+	if num_students % 2 == 1
+
+		if count < num_students - 3
+		puts "#{students[count]} & #{students[count + 1]}"
+		else
+		puts "#{students[count]}, #{students[count+1]}, & #{students[count+2]}"
+		break
+		end
+
+else 
+	puts "#{students[count]} & #{students[count+1]}"
+end 
 
 	count += 2
 
