560c12f49b80defd6143c892a7848b1af0146103
Geraldine <geraldinegalue@gmail.com>
2015-10-21 10:31:53 -0400

    paired up students

--- student_shuffle.rb
+++ student_shuffle.rb
@@ -1,6 +1,14 @@
 students = %w(Geraldine Janeth Gio Carlos Richard Jackeline Beatriz Angie Aaron)
-
 #%w(list items) makes an array
 
-puts students
-puts "#{students}"
+students.shuffle! 
+
+count = 0
+
+until count == (students.length - 1)
+
+	puts "#{students[count]} & #{students[count + 1]}"
+
+	count += 2
+
+end
